6da95e4c79d84cd65b2e6141883cfe9c3fd4ca05
Piyush varma <varmapiyush864@gmail.com>
2025-11-14 18:36:30 +0530

    Added all experiments

--- Modules/module2-backend/src/main/java/com/ecommerce/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
